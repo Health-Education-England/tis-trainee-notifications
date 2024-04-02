6aff8da80bedae78bca670d0edbb787c5c00fe8a
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2024-04-02 17:17:47 +0100

    feat: tests for dispatcher

---
+++ src/test/java/uk/nhs/tis/trainee/notifications/service/MessageDispatchServiceTest.java
@@ -0,0 +1,77 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2024 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.tis.trainee.notifications.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.web.client.RestTemplate;
+import uk.nhs.tis.trainee.notifications.model.MessageType;
+
+class MessageDispatchServiceTest {
+
+  private static final String SERVICE_URL = "the-url";
+  private static final String WHITELIST_1 = "123";
+  private static final String WHITELIST_2 = "456";
+  private static final List<String> WHITELISTED = List.of(WHITELIST_1, WHITELIST_2);
+
+  private RestTemplate restTemplate;
+  private MessageDispatchService service;
+
+  @BeforeEach
+  void setUp() {
+    restTemplate = mock(RestTemplate.class);
+    service = new MessageDispatchService(restTemplate, WHITELISTED, false, false, SERVICE_URL);
+  }
+
+  @ParameterizedTest
+  @EnumSource(MessageType.class)
+  void whitelistedShouldBeValidRecipients(MessageType messageType) {
+    assertThat("Unexpected isValidRecipient().",
+        service.isValidRecipient(messageType, WHITELIST_1), is(true));
+  }
+
+  @ParameterizedTest
+  @EnumSource(MessageType.class)
+  void nonWhitelistedShouldNotBeValidRecipientsIfNotificationsNotEnabled(MessageType messageType) {
+    assertThat("Unexpected isValidRecipient().",
+        service.isValidRecipient(messageType, "some other id"), is(false));
+  }
+
+  @Test
+  void nonWhitelistedShouldBeValidRecipientsIfNotificationsEnabled() {
+    service = new MessageDispatchService(restTemplate, WHITELISTED, true, false, SERVICE_URL);
+    assertThat("Unexpected isValidRecipient().",
+        service.isValidRecipient(MessageType.IN_APP, "some other id"), is(true));
+
+    service = new MessageDispatchService(restTemplate, WHITELISTED, false, true, SERVICE_URL);
+    assertThat("Unexpected isValidRecipient().",
+        service.isValidRecipient(MessageType.EMAIL, "some other id"), is(true));
+  }
+
+}
