15e95a43509235ab768907b34e03464aef788d30
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2024-10-08 17:08:06 +0100

    fix: unused import

--- src/test/java/uk/nhs/tis/trainee/notifications/service/NotificationServiceTest.java
+++ src/test/java/uk/nhs/tis/trainee/notifications/service/NotificationServiceTest.java
@@ -35,7 +35,6 @@ import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
