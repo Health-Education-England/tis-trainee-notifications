b96d42988c4b09022a8e01fc020a028eb7d7aad7
Reuben Roberts <reuben.roberts@nhs.net>
2025-07-22 14:33:16 +0100

    notifications and tests

---
+++ src/main/java/uk/nhs/tis/trainee/notifications/model/ProgrammeActionType.java
@@ -0,0 +1,13 @@
+package uk.nhs.tis.trainee.notifications.model;
+
+/**
+ * The type category of the programme action to be performed.
+ */
+public enum ProgrammeActionType {
+
+  SIGN_COJ,
+  SIGN_FORM_R_PART_A,
+  SIGN_FORM_R_PART_B,
+  REGISTER_TSS
+
+}
