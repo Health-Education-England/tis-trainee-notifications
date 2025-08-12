eb0a1d14348e2663226399d45fbcd8d3afd8f224
Reuben Roberts <reuben.roberts@nhs.net>
2025-08-12 16:23:09 +0100

    fix: rest call type preservation

---
+++ src/test/java/uk/nhs/tis/trainee/notifications/event/ProgrammeMembershipListenerIntegrationTest.java
@@ -0,0 +1,11 @@
+package uk.nhs.tis.trainee.notifications.event;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+public class ProgrammeMembershipListenerIntegrationTest {
+}
