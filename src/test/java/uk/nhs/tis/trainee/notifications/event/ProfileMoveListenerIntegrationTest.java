059f20b95f83f8d1a211f9c510b140b12f6548ef
Reuben Roberts <reuben.roberts@nhs.net>
2025-10-10 09:51:01 +0100

    fix: code smell

--- src/test/java/uk/nhs/tis/trainee/notifications/event/ProfileMoveListenerIntegrationTest.java
+++ src/test/java/uk/nhs/tis/trainee/notifications/event/ProfileMoveListenerIntegrationTest.java
@@ -45,7 +45,6 @@ import java.util.UUID;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
