519176f444ed64ae96b6aade4c730c7263526e2a
Reuben Roberts <reuben.roberts@nhs.net>
2026-07-07 13:34:47 +0100

    last try on cooldown test

--- src/test/java/uk/nhs/tis/trainee/notifications/event/LtftListenerIntegrationTest.java
+++ src/test/java/uk/nhs/tis/trainee/notifications/event/LtftListenerIntegrationTest.java
@@ -148,7 +148,7 @@ class LtftListenerIntegrationTest {
         () -> LTFT_UPDATED_ASSIGNMENT_QUEUE);
     registry.add("application.email.enabled", () -> true);
     registry.add("application.domain", () -> URI.create("https://test.test.test"));
-    registry.add("application.ltft.assignment-cooldown", () -> "PT30S");
+    registry.add("application.ltft.assignment-cooldown", () -> "PT5S");
 
     registry.add("spring.cloud.aws.region.static", localstack::getRegion);
     registry.add("spring.cloud.aws.credentials.access-key", localstack::getAccessKey);
@@ -1142,8 +1142,8 @@ class LtftListenerIntegrationTest {
         .ignoreExceptions()
         .untilAsserted(() -> verify(mailSender).send(messageCaptor.capture()));
 
-    // Manually delete cooldown key to simulate expiry (avoids slow sleep).
-    redisTemplate.delete(LtftService.ASSIGNMENT_COOLDOWN_KEY_PREFIX + adminEmail);
+    // Wait for the 5-second cooldown to expire.
+    Thread.sleep(5500);
 
     // Reset mock to verify the next send independently.
     reset(mailSender);
